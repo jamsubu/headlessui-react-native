2f8fcb1d5a7367c0833f7f287726d3d6080daf3a
nakzyu <nakzyu@gmail.com>
2024-05-08 12:58:49 +0900

    feat: remove dev app, add showcase app, import packages in showcase app

---
+++ apps/showcase/app/(tabs)/index.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet, View } from "react-native";
+
+export default function HomeScreen() {
+  return <View></View>;
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: "absolute",
+  },
+});
