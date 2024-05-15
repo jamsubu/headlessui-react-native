bda761073dbf65c800e168178dafcdf78d1a92a4
nakzyu <nakzyu@gmail.com>
2024-05-15 16:49:10 +0900

    feat: replace root dir if dev env

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,11 +1,24 @@
 import nextra from "nextra";
+import path from "path";
 import remakrCodeImport from "remark-code-import";
+
+const currentDir = process.cwd();
+
+const isDev = process.env.NODE_ENV === "development";
 
 const withNextra = nextra({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.jsx",
   mdxOptions: {
-    remarkPlugins: [[remakrCodeImport, { allowImportingFromOutside: true }]],
+    remarkPlugins: [
+      [
+        remakrCodeImport,
+        {
+          allowImportingFromOutside: true,
+          rootDir: isDev ? path.resolve(currentDir, "..", "..") : currentDir,
+        },
+      ],
+    ],
   },
 });
 
