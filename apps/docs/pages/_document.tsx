10196e44e93030c6a5c33c928cd8c215287ce621
nakzyu <59201980+nakzyu@users.noreply.github.com>
2024-06-13 15:25:54 +0900

    add google search console meta tag

---
+++ apps/docs/pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="uNf_pjnoPY77W-MI0arlXSAOKt6zglU_dp9tJvpqEQ8"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
