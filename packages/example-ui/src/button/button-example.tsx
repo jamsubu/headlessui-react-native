2b02336a7cc27f5b11c970d4c1e68dc6cd8ad293
Dann1y <leedanny0102@gmail.com>
2024-06-28 23:45:26 +0900

    feat(ui): add button component's initial example

---
+++ packages/example-ui/src/button/button-example.tsx
@@ -0,0 +1,10 @@
+import { View, ViewStyle } from "react-native";
+import { Button } from "headlessui-react-native";
+
+export function ButtonExample() {
+  return (
+    <View>
+      <Button title="hello" />
+    </View>
+  );
+}
